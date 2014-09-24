2e2e91f979853045811bbabccc3bfe37a75cbccc
Jonathan Aceituno <join@glencoe.lifl.fr>
2014-09-24 16:48:24 +0200

    Anyway, it works

--- vagrant-bootstrap.sh
+++ vagrant-bootstrap.sh
@@ -4,8 +4,9 @@ apt-get update
 apt-get -y install software-properties-common python-software-properties git build-essential
 apt-get -y install gcc-arm-none-eabi gdb-arm-none-eabi binutils-arm-none-eabi openocd
 mkdir /tp
-chown vagrant /tp
 git clone https://gitorious.org/uc-sdk/uc-sdk.git /tp/uC-sdk
 wget http://thomaspietrzak.com/enseignement/NIHM/Makefile -O /tp/Makefile
-wget http://thomaspietrzak.com/enseignement/NIHM/test.c -O /tp/test.c
-echo "The dev environment is at /tp"
+ln -s /vagrant/src /tp/src
+chown -R vagrant /tp
+chgrp -R vagrant /tp
+echo "The dev environment is at /tp. The source file is /tp/src/test.c."
