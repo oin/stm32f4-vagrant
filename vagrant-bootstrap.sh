9b663d53003e0f9d2cbfe98d0b0eb6f1a759519b
Matthieu <matthieu.falce@inria.fr>
2014-10-06 13:56:51 +0200

    vagrant doesn't do shell expansion and added byobu

--- vagrant-bootstrap.sh
+++ vagrant-bootstrap.sh
@@ -3,14 +3,14 @@
 apt-get update
 apt-get -y install software-properties-common python-software-properties git build-essential
 apt-get -y install gcc-arm-none-eabi gdb-arm-none-eabi binutils-arm-none-eabi openocd
-apt-get -y install lolcat cowsay
+apt-get -y install lolcat cowsay byobu
 mkdir /tp
 git clone https://gitorious.org/uc-sdk/uc-sdk.git /tp/uC-sdk
 
 #wget http://thomaspietrzak.com/enseignement/NIHM/Makefile -O /tp/Makefile
 wget https://raw.githubusercontent.com/ice3/stm32f4-vagrant/master/Makefile -O /tp/Makefile
-wget https://raw.githubusercontent.com/ice3/stm32f4-vagrant/master/.bashrc  -O ~/.bashrc
+wget https://raw.githubusercontent.com/ice3/stm32f4-vagrant/master/.bashrc  -O /home/vagrant/.bashrc
 ln -s /vagrant/src /tp/src
 chown -R vagrant /tp
 chgrp -R vagrant /tp
-echo "The dev environment is at /tp. The source file is /tp/src/test.c."
+echo "The dev environment is at /tp. The source file is /tp/src/test.c.
