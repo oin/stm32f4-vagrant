f5f9ec8ad33da2b02159915b6faaea385105b751
Matthieu <matthieu.falce@inria.fr>
2014-10-06 13:47:05 +0200

    added a new bashrc file with a message telling if a ST device is connected

--- vagrant-bootstrap.sh
+++ vagrant-bootstrap.sh
@@ -3,9 +3,13 @@
 apt-get update
 apt-get -y install software-properties-common python-software-properties git build-essential
 apt-get -y install gcc-arm-none-eabi gdb-arm-none-eabi binutils-arm-none-eabi openocd
+apt-get -y install lolcat cowsay
 mkdir /tp
 git clone https://gitorious.org/uc-sdk/uc-sdk.git /tp/uC-sdk
-wget http://thomaspietrzak.com/enseignement/NIHM/Makefile -O /tp/Makefile
+
+#wget http://thomaspietrzak.com/enseignement/NIHM/Makefile -O /tp/Makefile
+wget https://raw.githubusercontent.com/ice3/stm32f4-vagrant/master/Makefile -O /tp/Makefile
+wget https://raw.githubusercontent.com/ice3/stm32f4-vagrant/master/.bashrc  -O ~/.bashrc
 ln -s /vagrant/src /tp/src
 chown -R vagrant /tp
 chgrp -R vagrant /tp
